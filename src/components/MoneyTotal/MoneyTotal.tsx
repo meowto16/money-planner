dd6fa40eaac1afe38fd0bd99b645afb87d14cb2b
Maxim Kirshin <meowto16@gmail.com>
2022-05-03 22:20:18 +0500

    feature: adds spent alert

--- src/components/MoneyTotal/MoneyTotal.tsx
+++ src/components/MoneyTotal/MoneyTotal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Alert } from '@mui/material'
+import { Alert, Box, Stack } from '@mui/material'
 import { useSelector } from 'react-redux'
 
 import { moneySelectors } from '../../store/money.slice'
@@ -7,13 +7,23 @@ import numberFormat from '../../utils/numberFormat'
 
 const MoneyTotal = () => {
   const totalMoneyCalculated = useSelector(moneySelectors.getTotalMoneyCalculated)
+  const moneySpent = useSelector(moneySelectors.getCostsSum)
 
   return (
-    <Alert severity={totalMoneyCalculated < 0 ? 'error' : 'info'} icon={false}>
-            Остается денег: {typeof totalMoneyCalculated === 'number'
-        ? `${numberFormat.format(totalMoneyCalculated)} руб.`
-        : 'Не указано'}
-    </Alert>
+    <Box>
+      <Stack sx={{ width: '100%' }} spacing={1}>
+        <Alert severity={totalMoneyCalculated < 0 ? 'error' : 'info'} icon={false}>
+          Остается денег: {typeof totalMoneyCalculated === 'number'
+            ? `${numberFormat.format(totalMoneyCalculated)} руб.`
+            : 'Не указано'}
+        </Alert>
+        <Alert severity={totalMoneyCalculated < 0 ? 'error' : 'info'} icon={false}>
+          Потрачено денег: {typeof moneySpent === 'number'
+            ? `${numberFormat.format(moneySpent)} руб.`
+            : 'Не указано'}
+        </Alert>
+      </Stack>
+    </Box>
   )
 }
 
