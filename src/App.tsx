7309f3b46f6185e73450ebf53b687da19f7851d9
Maxim Kirshin <meowto16@gmail.com>
2022-05-22 06:36:40 +0500

    feature: removes unused EntryScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 
-import { moneySelectors } from './store/money.slice'
-
-import EntryScreen from './screens/EntryScreen/EntryScreen'
 import CalculateScreen from './screens/CalculateScreen/CalculateScreen'
 
-const { getTotalMoney } = moneySelectors
-
 const App: React.FC = () => {
-  const money = useSelector(getTotalMoney)
-
-  return money === null ? <EntryScreen /> : <CalculateScreen />
+  return <CalculateScreen />
 }
 
 export default App
