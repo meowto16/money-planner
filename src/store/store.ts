c3cd2c8452f645d766631089bd68fcfd3c38a64d
Maxim Kirshin <meowto16@gmail.com>
2022-06-03 02:22:48 +0500

    feature: adds redux-devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,5 @@
-import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
-import { combineReducers } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 import { moneyReducer, MoneyState } from './money.slice'
@@ -20,5 +19,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const store = createStore(persistedReducer)
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV === 'development',
+})
 export const persistor = persistStore(store)
